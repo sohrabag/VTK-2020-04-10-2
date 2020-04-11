ccaf460b49f5e5162bf9063ffee6aa90d0003a8c
sag sag <sohrabag@kth.se>
2020-04-11 20:46:10 +0200

    childA index.html parent.java

---
+++ VTK1/parent.java
@@ -0,0 +1,12 @@
+public class Parent
+{
+
+    public static void main(String[] args)
+    {
+       System.out.println("This is parent class");
+       System.out.println("I switched back to inheritance branch");
+       System.out.println("I am doing some changes here and then go 
+back to master branch again");
+    }
+
+}
